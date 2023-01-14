a67b2aa31f623edfaf017bac815c3c4643c322ca
Alex <alexmarcus20141@gmail.com>
2023-01-14 21:57:11 +0200

    wip form & added mui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Form from './components/Form';
-import { Container } from '@mui/material'
+import { Container } from '@mui/material';
+import Box from '@mui/material/Box';
 
 function App() {
   return (
     <div className="App">
-      <Container maxWidth="sm">
-        <Form />
-      </Container>
+      <Box>
+        <Container maxWidth="sm">
+          <Form />
+        </Container>
+      </Box>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,55 @@
-import { Card, CardContent, Typography } from "@mui/material"
-import { Formik, Form, Field } from 'formik'
+import { Card, CardContent, Typography, } from "@mui/material"
+import { Formik, Form, Field, useFormik } from 'formik';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import { object, mixed, string } from 'yup';
 
+
+const validationSchema = object({
+  select: string().ensure().required("Select is required!")
+})
 
 const FormComponent = () => {
+  const formik = useFormik({
+    initialValues: {
+      select: ''
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values));
+    }
+  });
+
   return (
     <Card>
       <CardContent>
-        <Formik>
-          <Form>
-            <Field as="select" name="type">
-            </Field>
-          </Form>
-        </Formik>
+        <form onSubmit={formik.handleSubmit}>
+          <FormControl fullWidth>
+            <InputLabel id="select-label" name="select">Select</InputLabel>
+            <Select
+              labelId="select-label"
+              id="select"
+              value={formik.values.select}
+              label="Type"
+              onChange={formik.handleChange}
+              name="select"
+              error={Boolean(formik.errors.select)}
+              helpertext={formik.errors.select}
+            >
+              <MenuItem value={'article'}>Article</MenuItem>
+              <MenuItem value={'client'}>Client</MenuItem>
+              <MenuItem value={'monograf'}>Monograf</MenuItem>
+            </Select>
+          </FormControl>
+          <button
+            type="submit"
+            className="btn btn-primary btn-block"
+          >
+            Submit
+          </button>
+        </form>
       </CardContent>
     </Card>
   )
